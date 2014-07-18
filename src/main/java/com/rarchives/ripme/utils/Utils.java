fba8765cdfb9d3d79bf4d96cc04ae0ea89777a27
starb0bby <starb0bby.gs@gmail.com>
2014-07-18 14:01:13 -0400

    Started coding ForumRipper (vBulletin). Using ExposedForums as test site

--- src/main/java/com/rarchives/ripme/utils/Utils.java
+++ src/main/java/com/rarchives/ripme/utils/Utils.java
@@ -26,6 +26,11 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import com.rarchives.ripme.ripper.AbstractRipper;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.cookie.Cookie;
 
 /**
  * Common utility functions used in various places throughout the project.
@@ -35,6 +40,7 @@ public class Utils {
     public  static final String RIP_DIRECTORY = "rips";
     private static final String configFile = "rip.properties";
     private static final Logger logger = Logger.getLogger(Utils.class);
+    private static final boolean DEBUG_ON = true;
 
     private static PropertiesConfiguration config;
     static {
@@ -354,5 +360,43 @@ public class Utils {
             i = fullText.indexOf(start, j + finish.length());
         }
         return result;
+    }
+    
+        public static void printCookieStore(CookieStore cookieStore) {
+        debug("printCookies");
+        List<Cookie> cookies = cookieStore.getCookies();
+        if (cookies.isEmpty()) {
+            System.out.println("None");
+        } else {
+            for (int i = 0; i < cookies.size(); i++) {
+                System.out.println("- " + cookies.get(i).toString());
+            }
+        }
+        debug("end printCookies");
+    }
+
+    public static String md5(String pass) {
+        String md5 = "";
+        try {
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            byte[] data = pass.getBytes();
+            m.update(data, 0, data.length);
+            BigInteger i = new BigInteger(1, m.digest());
+            md5 = String.format("%1$032x", i);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e);
+        }
+        return md5;
+    }
+
+    public static long unixTime() {
+        return System.currentTimeMillis() / 1000L;
+    }
+
+    public static void debug(String msg) {
+        //TODO: add levels of debug
+        if (DEBUG_ON) {
+            System.out.println("Debug: " + msg);
+        }
     }
 }
